59c4f564661f32e67cd05903309f2f7735144835
Yojhan <ylrodriguez024@gmail.com>
2019-10-09 22:36:07 -0500

    Add first version vinculacion component

--- gateway/src/main/webapp/app/home/vinculacion/vinculacion.component.ts
+++ gateway/src/main/webapp/app/home/vinculacion/vinculacion.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'jhi-vinculacion',
   templateUrl: './vinculacion.component.html',
   styleUrls: ['./vinculacion.component.scss']
 })
-export class VinculacionComponent implements OnInit {
+export class VinculacionComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  constructor() { }
+  constructor(private elementRef: ElementRef, private renderer2: Renderer2) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = '#808080';
+    this.elementRef.nativeElement.ownerDocument.querySelector(".jh-card").style.padding = '0';
+  }
+
+  ngOnDestroy() {
+    this.renderer2.removeAttribute(this.elementRef.nativeElement.ownerDocument.body,'style')
+    this.renderer2.removeAttribute(this.elementRef.nativeElement.ownerDocument.querySelector(".jh-card"),'style')
+    
   }
 
 }
